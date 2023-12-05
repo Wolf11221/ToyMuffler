6960288bf449c75ee43171d4de867056c4b6af7b
Nie <76068682+Wolf11221@users.noreply.github.com>
2023-12-05 20:46:47 +0100

    init

---
+++ ToyMuffler/ToyMuffler/Patches/NoisemakerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace ToyMuffler.Patches
+{
+    [HarmonyPatch]
+    internal class NoisemakerPatch
+    {
+        [HarmonyPatch(typeof(NoisemakerProp), "ItemActivate")]
+        public static void Prefix(ref NoisemakerProp __instance)
+        {
+            __instance.minLoudness = 0f;
+            __instance.maxLoudness = 0f;
+        }
+    }
+}
---
+++ ToyMuffler/ToyMuffler/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using HarmonyLib;
+
+namespace ToyMuffler
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = "ToyMuffler",
+            AUTHOR = "Nie",
+            GUID = AUTHOR + "_" + MODNAME,
+            VERSION = "1.0.0";
+        
+        public static Harmony _harmony;
+        
+        public void Awake()
+        {
+            _harmony = new Harmony(GUID);
+            _harmony.PatchAll(typeof(Patches.NoisemakerPatch));
+        }
+    }
+}
